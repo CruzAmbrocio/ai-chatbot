f71d9f6178b24dd87fa8041a7436674731d941d7
CruzAmbrocio <cruzambrocio1409@gmail.com>
2023-12-22 14:10:56 -0600

    <BE> Token JWT generation and cookie storage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes";
+import cookieParser from "cookie-parser"
 
 const express = require("express");
 const cors = require("cors");
@@ -9,6 +10,7 @@ const app = express();
 config();
 
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev")); // TODO remove in production
 app.use("/api/v1", appRouter);
 app.use(cors());
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import Users from "../models/User";
 import User from "../models/User";
+import { createToken } from "../utilities/token-manager";
+import { COOKIE_NAME } from "../utilities/constants";
 
 const bcrypt = require("bcrypt");
 
@@ -24,12 +26,31 @@ export const userSignup = async (
 ) => {
   try {
     const { name, email, password } = req.body;
-    const existing = await User.findOne({email});
-    if( existing ) return res.status(401).send('User already exist');
+    const existing = await User.findOne({ email });
+    if (existing) return res.status(401).send("User already exist");
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new Users({ name, email, password:hashedPassword });
-    console.log(JSON.stringify(user))
+    const user = new Users({ name, email, password: hashedPassword });
+    console.log(JSON.stringify(user));
     await user.save();
+
+    // token generation and cookie creation
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error: any) {
     return res.status(200).json({ message: "ERROR", cause: error.message });
@@ -43,14 +64,33 @@ export const userLogin = async (
 ) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({email});
-    if( !user ) {
-      return res.status(401).send('User not registered');
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send("User not registered");
     }
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if( !isPasswordCorrect ) {
-      return res.status(403).send('Incorrect password');
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Incorrect password");
     }
+
+    // token generation and cookie creation
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error: any) {
     return res.status(200).json({ message: "ERROR", cause: error.message });
