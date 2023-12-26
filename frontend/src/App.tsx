0d0b583f10be460a9c1d1bbe1886342d163209b6
CruzAmbrocio <cruzambrocio1409@gmail.com>
2023-12-25 18:09:23 -0600

    <FE> Login page frontend design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/signup'
-import Chat from './pages/Chat'
-import NotFound from './pages/NotFound'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  console.log(useAuth()?.isLoggedIn);
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/chat" element={<Chat/>}/>
-      <Route path="/*" element={<NotFound/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,77 @@
+import { Box, Button, Typography } from "@mui/material";
+import CustomizeInput from "../components/shared/CustomizeInput";
+import { IoIosLogIn } from "react-icons/io";
+
 const Login = () => {
-  return <div>Login</div>;
+  const onSubmitHandle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+  };
+  return (
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="robot" style={{ width: "400px" }} />
+      </Box>
+      <Box
+        display={"flex"}
+        flex={{ xs: 1, md: 0.5 }}
+        alignItems={"center"}
+        padding={2}
+        ml={"auto"}
+        mt={16}
+      >
+        <form
+          onSubmit={onSubmitHandle}
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              textAlign="center"
+              padding={2}
+              fontWeight={600}
+            >
+              Login
+            </Typography>
+            <CustomizeInput type="email" name="email" label="email" />
+            <CustomizeInput type="password" name="password" label="password" />
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 1,
+                mt: 2,
+                width: "400px",
+                borderRadius: 2,
+                bgcolor: "#00fffc",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoIosLogIn />}
+            >
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
