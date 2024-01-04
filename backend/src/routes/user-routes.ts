1d9eb7759d19d5c4535f561c3ee1efd88db4cf54
CruzAmbrocio <cruzambrocio1409@gmail.com>
2024-01-04 00:53:46 -0600

    <BE> Token verification route and user verification

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getAllUsers, userLogin, userSignup } from "../controllers/user-controller";
+import { getAllUsers, userLogin, userSignup, verifyUser } from "../controllers/user-controller";
 import { loginValidator, signupValidator, validate } from "../utilities/validators";
+import { verifyToken } from "../utilities/token-manager";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), userSignup);
 userRoutes.post("/login", validate(loginValidator), userLogin);
+userRoutes.get("/auth-status", verifyToken, verifyUser);
 
 export default userRoutes;
