cb0d2159b55408ef97218b8dc7782cbd75eb7d25
CruzAmbrocio <cruzambrocio1409@gmail.com>
2024-01-04 13:02:54 -0600

    <BE> OpenAI routes and services created and configured chat retreival

--- backend/src/controllers/chats-controller.ts
+++ backend/src/controllers/chats-controller.ts
@@ -1,3 +1,42 @@
-export const getAllChats = ()=>{
+import { Request, Response, NextFunction } from "express";
+import User from "../models/User";
+import { configureOpenAI } from "../config/openai-config";
+import { ChatCompletionRequestMessage, OpenAIApi } from "openai";
 
-}
+export const generateChatCompletion = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { message } = req.body;
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "User not registered or token failed" });
+    }
+
+    const chats = user.chats.map(({ role, content }) => ({
+      role,
+      content,
+    })) as ChatCompletionRequestMessage[];
+    chats.push({ content: message, role: "user" });
+    user.chats.push({ content: message, role: "user" });
+
+    const config = configureOpenAI();
+    const openai = new OpenAIApi(config);
+    const chatResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: chats,
+    });
+    user.chats.push(
+      chatResponse.data.choices[0].message as ChatCompletionRequestMessage
+    );
+    await user.save();
+    return res.status(200).json({ chats: user.chats });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import userRoutes from "./user-routes";
 import { getAllUsers } from "../controllers/user-controller";
+import { chatCompletionValidator, validate } from "../utilities/validators";
+import { verifyToken } from "../utilities/token-manager";
+import { generateChatCompletion } from "../controllers/chats-controller";
 
 const chatsRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
+chatsRoutes.post(
+  "/new",
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion
+);
 
 export default chatsRoutes;
