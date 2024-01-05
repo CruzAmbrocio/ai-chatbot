160f1dce14dff0df7065c2a5a87325f97c32ab78
CruzAmbrocio <cruzambrocio1409@gmail.com>
2024-01-05 00:19:45 -0600

    <FE> Chat screen frame and boxing with dummy chats

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,5 +1,117 @@
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
+import { red } from "@mui/material/colors";
+const chatMessages = [
+  { role: "user", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+  { role: "assistant", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+  { role: "user", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+  { role: "assistant", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+  { role: "assistant", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+  { role: "user", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+  { role: "assistant", content: "hello w asdf asdf asdf asdf asdf asdf asdf " },
+];
 const Chat = () => {
-  return <div>Chat</div>;
+  const auth = useAuth();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBot
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask some questions related to knowledge, business, advices,
+            education, etc. but avoid sharing personal information.
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            CLEAR CONVERSATION
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{ textAlign: "center", fontSize: "40px", color: "white", mb: 2 }}
+        >
+          Model - GPT 3.5 turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+          {chatMessages.map((chat) => (
+            <div>{chat.content}</div>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Chat;
